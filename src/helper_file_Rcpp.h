4ac32d7cdb823e2d3ddc00cd0ebc9fa883fd5a74
kris-nader <naderkristenmichelle@gmail.com>
2023-05-23 23:44:52 +0300

    helper h file

---
+++ src/helper_file_Rcpp.h
@@ -0,0 +1,25 @@
+double rcpp_add(NumericVector v);
+
+NumericVector apply_cpp_col(NumericMatrix x );
+
+NumericVector apply_cpp_row_der(NumericMatrix x ):
+
+double rcpp_sum(NumericVector v);
+
+DataFrame apply_cpp_col_norm(NumericMatrix x );
+
+double cpp_med(NumericVector x);
+
+NumericVector apply_cpp_row_basel(NumericMatrix x );
+
+NumericVector apply_cpp_row_DR2(NumericMatrix x );
+
+NumericVector apply_cpp_row_temp(NumericMatrix x );
+
+NumericVector apply_cpp_row_cfh(NumericMatrix x );
+
+NumericVector apply_cpp_row_base(NumericMatrix x );
+
+NumericVector apply_cpp_col_tempa(NumericMatrix x );
+
+NumericVector apply_cpp_col_aj(NumericMatrix x ) ;
